55572d787aca7ffe927eb42f0a86066d6d73fed4
BhumitCoder <panchanibhumit@gmail.com>
2026-05-28 19:39:21 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -29,7 +29,6 @@ export default function robots(): MetadataRoute.Robots {
           "/admin",
           "/admin/",
           "/api/",
-          "/_next/",
           "/404",
           "/500",
         ],
